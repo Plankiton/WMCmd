d6bac9be2853e619150df6da6f5927c38b5b1f90
RoboCopGay <vinicios.sousa909@gmail.com>
2019-12-05 19:47:55 -0300

    menu type finished

--- i3cmd/menu/menu.c
+++ i3cmd/menu/menu.c
@@ -1,21 +1,58 @@
-#include <stdlib.h>
-#include "menu.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+
 #include "menu.h"
 
-Menu createMenu(int lenght){
-    Menu menu;
-    menu.lenght = 3;
-    menu.selected = 0;
+void createMenu(Menu * menu, int lenght, ...){
+    va_list items_list;
 
-    return menu;
+    // checking number of items on list
+    va_list check_items_list;
+    va_start(check_items_list, lenght);
+
+    int number_of_items = 0;
+    TEST test = va_arg(check_items_list, TEST);
+    while (!TEST_ISEQUAL(test, ITEM_NULL)){
+        number_of_items ++;
+        test = va_arg(check_items_list, TEST);
+    }
+
+    // creating menu
+    va_start(items_list, lenght);
+
+    menu->selected = 0;
+    menu->lenght = lenght;
+
+    menu->menu.items = malloc(sizeof(Item)*lenght);
+
+    int i = 0;
+    Item item = va_arg(items_list, Item);
+    while (i < lenght){
+        if (i < number_of_items){
+            menu->menu.items[i].value = item.value;
+            menu->menu.items[i].output = item.output;
+        } else {
+            menu->menu.items[i].value = "\0";
+            menu->menu.items[i].output = "\0";
+        }
+
+        i ++;
+        item = va_arg(items_list, Item);
+    }
+
+    va_end(items_list);
 }
 
 int main(int argc, char *argv[])
 {
-    Menu j = createMenu(3);
-    printf("%i", j.lenght);
+    Item joao = {"joao", "maria"};
+    Menu j;
+    createMenu(&j, 5, joao, joao, ITEM_NULL);
 
+    for (int i = 0; i<j.lenght; i++)
+        printf("%s: %s\n", j.menu.items[i]);
+
+    free(j.menu.items);
     return 0;
 }
--- i3cmd/menu/menu.h
+++ i3cmd/menu/menu.h
@@ -11,3 +11,21 @@ typedef struct {
     int lenght, selected;
     _menu menu;
 } Menu;
+
+typedef struct {
+    int __a;
+    int __b;
+    int __c;
+} TEST;
+
+TEST ITEM_NULL = {0, 0, 0};
+
+int TEST_ISEQUAL(TEST a, TEST b){
+    if (a.__a == b.__a){
+        if (a.__b == b.__b){
+            if(a.__c == b.__c){
+                return 1;
+            }else return 0;
+        }else return 0;
+    }else return 0;
+}
