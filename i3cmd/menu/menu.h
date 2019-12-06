e35a9486355a41d64e749d1808dfd393975a18c8
RoboCopGay <vinicios.sousa909@gmail.com>
2019-12-06 11:07:01 -0300

    organizing menu.h

--- i3cmd/menu/menu.h
+++ i3cmd/menu/menu.h
@@ -6,21 +6,3 @@ typedef union __menu {
     Item * items;
     union __menu * submenus;
 } Menu;
-
-typedef struct {
-    int __a;
-    int __b;
-    int __c;
-} TEST;
-
-TEST ITEM_NULL = {0, 0, 0};
-
-int TEST_ISEQUAL(TEST a, TEST b){
-    if (a.__a == b.__a){
-        if (a.__b == b.__b){
-            if(a.__c == b.__c){
-                return 1;
-            }else return 0;
-        }else return 0;
-    }else return 0;
-}
